739d19bab7df48a349ea6bd54daad5a2d2316ab7
RWC <39197176+readingwritingcode@users.noreply.github.com>
2021-06-23 11:06:49 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Copyright (c) Colav.
+# Distributed under the terms of the Modified BSD License.
+
+# -----------------------------------------------------------------------------
+# Minimal Python version sanity check (from IPython)
+# -----------------------------------------------------------------------------
+
+# See https://stackoverflow.com/a/26737258/2268280
+# sudo pip3 install twine
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+# For test purposes
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+import os
+import sys
+import codecs
+
+
+v = sys.version_info
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+shell = False
+if os.name in ('nt', 'dos'):
+    shell = True
+    warning = "WARNING: Windows is not officially supported"
+    print(warning, file=sys.stderr)
+
+
+def main():
+    setup(
+        # Application name:
+        name="TweetsCollector",
+
+        # Version number (initial):
+        version="0.1.0-beta",
+
+        # Application author details:
+        author="labcinfEIB",
+        author_email="david.medina1@udea.edu.co",
+
+        # Packages
+        packages=find_packages(exclude=['tests']),
+        python_requires=">=3.5",
+
+        # Include additional files into the package
+        include_package_data=True,
+
+        # Details
+        url="https://github.com/readingwritingcode/TweetCollect",
+        #scripts=[],
+        #
+        license="BSD",
+
+        description="Tweet Collector Automation Tools",
+
+        long_description=open("README.md").read(),
+
+        long_description_content_type="text/markdown",
+
+        # Dependent packages (distributions)
+        install_requires=[
+         
+           
+            'pandas',
+            
+        ],
+        classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: social scientist, data-Scientist',
+        'Topic :: twitter data :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
